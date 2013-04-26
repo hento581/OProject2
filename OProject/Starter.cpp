5ac3a5977c2599fc49a9864d031af425a6cdcc92
hento581 <henrik.tosteberg@gmail.com>
2013-04-26 11:37:32 +0200

    Removed unnecessary functions from starter

--- OProject/Starter.cpp
+++ OProject/Starter.cpp
@@ -74,114 +74,12 @@ GLfloat vertices[] = { -0.5f,-0.5f,0.0f,
 // vertex array object
 unsigned int vertexArrayObjID;
 
-
-Point3D findNormal(int x, int z, GLfloat *vertexArray, TextureData *tex)
-{
-	Point3D edge=vec3(0,1,0);
-	Point3D p0 = vec3(x,vertexArray[(x + z * tex->width)*3 + 1], z);
-	Point3D p1,p2,p3;
-
-	if(x > 0)
-		p1= SetVector(x-1, vertexArray[(x-1 + z * tex->width)*3 + 1], z);
-	else 
-		return edge;
-
-	if(z > 0)
-	{
-		p2 = SetVector(x-1, vertexArray[(x-1 + (z-1) * tex->width)*3 + 1], z-1);
-		p3 = SetVector(x, vertexArray[(x + (z-1) * tex->width)*3 + 1], z-1);
-	}
-	else 
-		return edge;
-	Point3D v1,v2,v3;
-	v1 = VectorSub(p1,p0);
-	v2 = VectorSub(p2,p0);
-	v3 = VectorSub(p3,p0);
-
-	
-	Point3D Nres,N1,N2,N3;
-
-	N1 = CrossProduct(v2,v1);
-	N2 = CrossProduct(v3,v2);
-	N3 = CrossProduct(v1,v3);
-
-	N1 = Normalize(N1);
-	N2 = Normalize(N2);
-	N3 = Normalize(N3);
-
-	Nres = VectorAdd(N1,N2);
-	Nres = VectorAdd(N3,Nres);
-
-	Nres = Normalize(Nres);
-	
-	return Nres;
-}
-
-
-
 // vertex array object
 Model *m, *m2, *tm;
 // Reference to shader program
 
 GLuint tex1, tex2,tex3,tex4;
 TextureData ttex; // terrain
-
-
-
-
-
-GLfloat interpolate(Point3D P, Point3D A, Point3D B, Point3D C,TextureData *tex)
-{
-	Point3D N,N1,N2;
-	int Ax = A.x;
-	int Az = A.z;
-	int Bx = B.x;
-	int Bz = B.z;
-	int Cx = C.x;
-	int Cz = C.z;
-
-
-	A.y = tex->imageData[(Ax + Az * tex->width) * (tex->bpp/8)] / 10.0;
-	B.y = tex->imageData[(Bx + Bz * tex->width) * (tex->bpp/8)] / 10.0;
-	C.y = tex->imageData[(Cx + Cz * tex->width) * (tex->bpp/8)] / 10.0;
-
-	N1 = VectorSub(C, A);
-	N2 = VectorSub(B, A);
-
-	N = CrossProduct(N1,N2);
-	if(N.y == 0)
-	return 100;
-
-	return A.y - (N.x*(P.x - A.x) + N.z*(P.z-A.z))/N.y;
-} 
-
-
-int insideTriangle(Point3D P, Point3D A, Point3D B, Point3D C)
-{
-	Point3D v0,v1,v2; 
-
-	v0 = VectorSub(C, A);
-	v1 = VectorSub(B, A);
-	v2 = VectorSub(P, A);
-
-
-	// Compute dot products
-	GLfloat  dot00 = DotProduct(v0, v0);
-	GLfloat  dot01 = DotProduct(v0, v1);
-	GLfloat  dot02 = DotProduct(v0, v2);
-	GLfloat  dot11 = DotProduct(v1, v1);
-	GLfloat  dot12 = DotProduct(v1, v2);
-
-	// Compute barycentric coordinates
-	GLfloat invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-	GLfloat u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-	GLfloat v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-
-	// Check if point is in triangle
-	if((u >= 0) && (v >= 0) && (u + v < 1))
-	return 1;
-	return 0;
-} 
 
 
 void keyboardFunction (unsigned char key, int xmouse, int ymouse)
