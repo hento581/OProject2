747e61440938c0e222cea8c3ba809ed93cd037a5
oskda283 <oskar_daniels@hotmail.com>
2013-04-29 11:41:10 +0200

    la till billboardModel(void)

--- OProject/Starter.cpp
+++ OProject/Starter.cpp
@@ -73,12 +73,44 @@ GLfloat vertices[] = { -0.5f,-0.5f,0.0f,
 unsigned int vertexArrayObjID;
 
 // vertex array object
-Model *m, *m2, *tm;
+Model *m, *m2, *tm, *bill;
 // Reference to shader program
 
 GLuint tex1, tex2,tex3,tex4;
 TextureData ttex; // terrain
 
+Model* billboardModel(void)
+{
+	int vertexCount = 4;
+	int triangleCount = 2;
+	
+	GLfloat vertexArray[] = { -0.5f,2.0f,0.0f,
+						     0.5f,2.0f,0.0f,
+						    -0.5f,0.0f,0.0f,
+							 0.5f,0.0f,0.0f};
+	GLfloat normalArray[] = { 0.0f,0.0f,1.0f,
+						     0.0f,0.0f,1.0f,
+						     0.0f,0.0f,1.0f,
+							 0.0f,0.0f,1.0f,};
+	GLfloat texCoordArray[] = { 1.0f,0.0f,
+								1.0f,1.0f,
+								0.0f,0.0f,
+								0.0f,1.0f};
+
+	GLuint indexArray[] = { 1,2,3,
+							2,3,4};
+
+	Model* m = LoadDataToModel(
+			vertexArray,
+			normalArray,
+			texCoordArray,
+			NULL,
+			indexArray,
+			vertexCount,
+			triangleCount*3);
+
+	return m;
+}
 
 void keyboardFunction (unsigned char key, int xmouse, int ymouse)
 {	
@@ -205,6 +237,7 @@ void init(void)
 	world = new World(&ttex);
 	player = new Player(p,l,world);
 	tm = world->GenerateTerrain();
+	bill = billboardModel();
 	printError("init terrain");
 
 }
@@ -300,7 +333,7 @@ void display(void)
 
 	// Build matrix
 	
-	modelView= IdentityMatrix();
+	modelView= T(2.0,2.0,2.0);
 
 	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, modelView.m);
 	glUniformMatrix4fv(glGetUniformLocation(program, "camMatrix"), 1, GL_TRUE, player->getCamMatrix().m);
@@ -315,7 +348,7 @@ void display(void)
 	glUniform1i(glGetUniformLocation(program, "tex2"), 2);
 	glUniform1i(glGetUniformLocation(program, "tex3"), 3);
 	DrawModel(tm, program, "inPosition", "inNormal", "inTexCoord");
-	
+	DrawModel(bill, program, "inPosition", "inNormal", "inTexCoord");
 	translate=  T(gx, world->findHeight(gx, gz), gz);
 	total = Mult(modelView, translate);
 
